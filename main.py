43757c2e1c6c545f6a07fbe3aaccf8dea69cc341
Haze Nexus <suporte.haze.nexus@gmail.com>
2025-10-05 12:28:08 -0300

    First commit haze bot

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv("TOKEN")
+print(f"Token carregado: {token}")
+
+permissoes= discord.Intents.default()
+permissoes.message_content = True
+permissoes.members = True
+HazeNexus = commands.Bot(command_prefix='mateus.gayzao', intents=permissoes)
+
+@HazeNexus.command()
+async def ola(ctx):
+    await ctx.send('Olá! Eu sou o Haze Nexus, seu assistente virtual. Como posso ajudar você hoje?')
+
+
+@HazeNexus.event
+async def on_ready():
+    print("Estou Pronto!")
+
+HazeNexus.run(token) 
